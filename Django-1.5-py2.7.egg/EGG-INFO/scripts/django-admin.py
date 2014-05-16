df0bc94f3897213dec7534308d7aa4a67bf2edc0
Marcio <marcio.cruz@ispm.com.br>
2014-05-16 16:00:41 -0300

    atualizando django para 1.5.7

--- Django-1.5-py2.7.egg/EGG-INFO/scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/mportela/nv3/venv_1.5/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
---
+++ Django-1.5.7-py2.7.egg/EGG-INFO/scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/vagrant/env2/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
